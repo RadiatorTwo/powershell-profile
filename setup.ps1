0ccea3192eb4d4ad45abfa2fae3f99293d609a1c
RadiatorTwo <radiatortwo@gmail.com>
2024-12-21 12:37:53 +0100

    Changed Font to Hack NF

--- setup.ps1
+++ setup.ps1
@@ -108,7 +108,7 @@ catch {
 }
 
 # Font Install
-Install-NerdFonts -FontName "CascadiaCode" -FontDisplayName "CaskaydiaCove NF"
+Install-NerdFonts -FontName "Hack" -FontDisplayName "Hack NF"
 
 # Final check and message to the user
 if ((Test-Path -Path $PROFILE) -and (winget list --name "OhMyPosh" -e) -and ($fontFamilies -contains "CaskaydiaCove NF")) {
